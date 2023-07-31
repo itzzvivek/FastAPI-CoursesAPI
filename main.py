6560571d76662ecafa98b46f055cce5587cb90de
Vivek Warkade <vivekwarkade000@gmail.com>
2023-07-31 17:32:04 +0530

    course_id & chapter_id

--- main.py
+++ main.py
@@ -44,3 +44,29 @@ def get_courses(sort_by: str = 'date', domain: str = None):
 
     courses = db.courses.find(query, {'name': 1, 'date': 1, 'description': 1, 'domain':1,'rating':1,'_id': 0}).sort(sort_field, sort_order)
     return list(courses)
+
+
+@app.get('/courses/{course_id}')
+def get_course(course_id: str):
+    course = db.courses.find_one({'_id': ObjectId(course_id)}, {'_id': 0, 'chapters': 0})
+    if not course:
+        raise HTTPException(status_code=404, detail='Course not found')
+    try:
+        course['rating'] = course['rating']['total']
+    except KeyError:
+        course['rating'] = 'Not rated yet' 
+    
+    return course
+
+
+@app.get('/courses/{course_id}/{chapter_id}')
+def get_chapter(course_id: str, chapter_id: str):    
+    course = db.courses.find_one({'_id': ObjectId(course_id)}, {'_id': 0, })
+    if not course:
+        raise HTTPException(status_code=404, detail='Course not found')
+    chapters = course.get('chapters', [])
+    try:
+        chapter = chapters[int(chapter_id)]
+    except (ValueError, IndexError) as e:
+        raise HTTPException(status_code=404, detail='Chapter not found') from e
+    return chapter
